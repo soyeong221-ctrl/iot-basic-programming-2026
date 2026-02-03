bc2504548ea3388e8664174cc98199c5694083f9
KIjoo2024 <soyeong221@gmail.com>
2026-02-03 09:20:04 +0900

    Reapply "1일차 수강완료"
    This reverts commit daa6e05eb968411a81163f7a3f76a6676f833fd4.

---
+++ Day01/Chap01-Solution/Chap01-02-app/Chap01-02-app.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+
+int main(void) 
+{
+	printf("I want to go to Angkor Wat!");
+	return 0;
+}
---
+++ Day01/Chap02-Solution/Chap02-02-app/main.c
@@ -0,0 +1,43 @@
+﻿// Chap02-02-app : 탈출문자(\ Escape Character), 변환문자(%) 학습
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!\n");   // 콘솔에 문자열을 format에 맞춰서 출력하는 함수
+
+	// \n : escape character -> 한 줄 내림 : newline
+
+	printf("12345\n");
+	printf("67890\n");
+	printf("WoW\n");
+
+	// '\t' -> 콘솔에 탭 추가 : tab
+	printf("My friend, Puka.\n");
+	printf("My\tfriend,\tPuka.\n");
+
+	// \b -> 백스페이스 : backspace
+	printf("Goot\bd Morning!\n");
+
+	// \a -> 알람소리 : alarm
+	printf("Bark!!\a\n");
+
+	// \r -> 글자 쓰는 맨 앞으로 이동 : returncarrige
+	printf("Department\rLotte\n");
+
+	// 숫자를 콘솔에 출력하는 기능
+	// %d -> decimal : 정수를 문자로 출력
+	printf("%d\n", 10);
+	// %nd -> n의 자리수만큼 늘려서 출력
+	printf("%10d\n", 10);
+	// %f -> float : 실수를 문자로 출력
+	printf("%f\n", 3.141592);
+	// %lf -> long float : 소수점을 6자리까지 출력
+	printf("%lf\n", 3.14);
+	// %.nf -> 소수점 n자리까지 출력하고, 그 뒤는 반올림
+	printf("%.2f\n", 3.14159265727);
+
+	// 여러개의 파라미터
+	printf("%5d x %5d = %5d", 6, 7, 6 * 7);
+
+
+	return 0;
+}
